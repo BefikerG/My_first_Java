6b47fb2b71dba26be030c287d6d605c7b9303905
Befiker G <94683293+bfg101@users.noreply.github.com>
2023-11-17 01:50:20 +0300

    Add files via upload

---
+++ Java Exercises/My_first_JAVA_program5/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+        public static void main(String[] args) {
+                // Displaying name, address, and telephone number
+                System.out.println("Name: Befiker Gezahegn");
+                System.out.println("Address: Addis Ababa");
+                System.out.println("Telephone Number: +251946685829");
+        }
+}
